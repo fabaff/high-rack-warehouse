abf2108011fcab57df62bf4d25444ceb92c958c8
Fabian Affolter <mail@fabian-affolter.ch>
2013-11-02 11:21:55 +0100

    add initial class for the calculation of movements

---
+++ RackSimSoft/src/calculation/Movement.java
@@ -0,0 +1,13 @@
+package calculation;
+
+/**
+ * The operating unit is moving inside the gap in the direction of the 
+ * y axis and its beam is only able to move in the direction of the z axis.
+ * The superposition of both moving vectors let the operating unit move
+ * inclined in side the yz area of the grid.
+ */
+
+public class Movement {
+	
+	
+}
