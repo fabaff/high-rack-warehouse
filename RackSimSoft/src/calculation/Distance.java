e417607d71bacb5eb8828ee8f0f1fa8e15fee4ea
Fabian Affolter <mail@fabian-affolter.ch>
2013-11-03 15:06:48 +0100

    add dist to zero

--- RackSimSoft/src/calculation/Distance.java
+++ RackSimSoft/src/calculation/Distance.java
@@ -13,6 +13,21 @@ public class Distance {
 	private int zDistance;
 	
 	/**
+	 * Calculates the distance of a bin to the zero point in the grid.
+	 * 
+	 * @param location
+     * @param binID
+	 */
+	public double zeroDistance(Location location, String binID)
+	{
+		Bin bin;
+		bin = location.getBin(binID);
+		double zeroDistance = Math.round(Math.sqrt(Math.pow((bin.getY()), 2) + 
+				Math.pow((bin.getZ()), 2)));
+		return zeroDistance;
+	}
+	
+	/**
 	 * Calculates the y distance of two bins in the grid.
 	 * 
 	 * @param location
@@ -60,7 +75,7 @@ public class Distance {
 		Bin bin;
 		bin = location.getBin(binID);
 		
-		int xDistance = gap.getWidth() / 2 + bin.getWidth() / 2;
+		int xDistance = (gap.getWidth() / 2) + (bin.getWidth() / 2);
 		return xDistance;
 	}
 	
@@ -79,7 +94,7 @@ public class Distance {
 		bin1 = location.getBin(binID1);
 		bin2 = location.getBin(binID2);
 		// The result is rounded.
-		double mDistance = Math.round(Math.sqrt(Math.pow((bin1.getY() + 
+		double mDistance = Math.round(Math.sqrt(Math.pow((bin1.getY() - 
 				bin2.getY()), 2) + Math.pow((bin1.getZ() - bin2.getZ()), 2)));
 		return mDistance;
 	}
