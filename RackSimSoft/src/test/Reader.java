b1672fc1d7941ca9defdcbd3b8aef01a607a1a62
Fabian Affolter <mail@fabian-affolter.ch>
2013-11-15 15:04:08 +0100

    update

--- RackSimSoft/src/test/Reader.java
+++ RackSimSoft/src/test/Reader.java
@@ -6,16 +6,16 @@ import helper.*;
 public class Reader {
 
 	public static void main(String[] args) throws IOException {
-		String article_list = "article_list.txt";
-		
-		
-		
 		System.out.println("Reading files and printing the content:");
-		
-		ReadingFiles reader = new ReadingFiles();
-	    reader.readArticles(article_list);
-	    
-	    
-	}
 
+		// Articles
+		String article_file = "article_list.txt";
+		ReadingFiles readerArticles = new ReadingFiles();
+	    readerArticles.readArticles(article_file);
+	    
+	    // Location
+		//String location_file = "location.xml";
+		//ReadingFiles readerLocation = new ReadingFiles();
+	    //readerLocation.readLocation(location_file);
+	}
 }
