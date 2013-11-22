fd5f627ee14c05c20bab459b7192547e38663a94
Fabian Affolter <mail@fabian-affolter.ch>
2013-11-22 17:19:01 +0100

    add initial location reader

--- RackSimSoft/src/helper/ReadingFiles.java
+++ RackSimSoft/src/helper/ReadingFiles.java
@@ -10,6 +10,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import location.*;
+
 /**
  * Reading data from external files as input for creating of all needed
  * elements.
@@ -44,11 +46,14 @@ public class ReadingFiles {
 					errors.add("Error on line " + line);
 				}
 			}			
-			if (errors.size() != 0) {
-			for (String s : errors)
-			    System.out.println(s);
+			if (errors.size() != 0)
+			{
+				for (String s : errors)
+				{
+					System.out.println(s);
+				}
 			}
-			System.out.println("Total processed lines: " + line);
+			System.out.println("Total processed lines: " + line + "\n");
 	    }
 	}
 
@@ -65,16 +70,118 @@ public class ReadingFiles {
 		 * Grid [2];<ID>;<U-Size>
 		 * Column [1..*];<ID>;<Y-Size>
 		 * Row [1..*];<ID>;<Z-Size>
+		 * 
+		 * Sample data:
+		 * 	Location;my location;3;MM
+			Gap;Gasse1;1000;Grid1;Grid2;7;4
+			Grid;Grid1;1000
+			Grid;Grid2;1000
+			Column;A;1200
+			Column;B;800
+			Column;C;800
+			Column;D;1200
+			Column;E;1000
+			Column;F;1000
+			Column;G;1200
+			Row;1;600
+			Row;2;800
+			Row;3;500
+			Row;4;600
 		 */
 		Path path = Paths.get(FileName);
+		Location myLocation;
+
+		int line = 0;
+		ArrayList<String> errors = new ArrayList<String>();
 		try (Scanner scanner =  new Scanner(path, ENCODING.name()))
 		{
-			while (scanner.hasNextLine())
-			{
-				// Code goes here...
-				log(scanner.nextLine());
-			}      
+			while (scanner.hasNextLine()){
+				String parts[];
+				String string = scanner.nextLine();
+				if (string.contains(";"))
+				{
+					//System.out.println(string);
+					
+					parts = string.split(";");
+					line++;
+
+					if (parts[0].equals("Location"))
+					{
+						System.out.println("--- Location created");
+						Location.MeasurementUnit measurementUnit;
+					
+						switch (parts[3])
+						{
+							case "MM":
+								measurementUnit = Location.MeasurementUnit.MM;
+								break;
+							case "CM":
+								measurementUnit = Location.MeasurementUnit.CM;
+								break;
+							case "DM":
+								measurementUnit = Location.MeasurementUnit.DM;
+								break;
+							case "M":
+								measurementUnit = Location.MeasurementUnit.M;
+								break;
+							default:
+								measurementUnit = Location.MeasurementUnit.MM;
+						}
+						myLocation = Location.getInstance(parts[1], measurementUnit);
+					}
+					
+					if (parts[0].equals("Gap"))
+					{
+						System.out.println("--- Gap created");
+						Gap gap = new Gap(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+
+					}
+					
+					if (parts[0].equals("Grid"))
+					{
+						System.out.println("---Grid");
+					}
+					
+					if (parts[0].equals("Column"))
+					{
+						System.out.println("---Column");
+					}
+					
+					if (parts[0].equals("Row"))
+					{
+						System.out.println("---Row");
+					}
+
+
+				} else {
+					errors.add("Error on line " + line);
+				}
+			}			
+			if (errors.size() != 0) {
+				for (String s : errors)
+				{
+					System.out.println(s);
+				}
+			}
+			System.out.println("Total processed lines: " + line);
 	    }
+				/*
+				Location
+				Location myLocation = Location.getInstance("My Location", measurementUnit);
+				Grid
+				Gap gap = new Gap("" + i, width, (i * 2 * depth) + depth + (i * width));
+				
+				
+				columnArray[k] = new Column("" + k, length, k * length);  // id, width, y coordinate
+				rowArray[k] = new Row("" + k, height, k * height);  // id, height, z coordinate
+
+				Grid grid = new Grid("" + gridID, gap, j % 2, columnArray, rowArray, depth);
+				gap.addGrid(grid);
+				gridID ++;
+				
+				myLocation.addGap(gap);
+
+				*/
 	}
 	    
 	/**
