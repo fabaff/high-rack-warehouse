5ccbb05abe3522e0b428a5f3393c670b7f10faf1
vanoa1 <vanoa1@bfh.ch>
2013-12-13 13:49:22 +0100

    GUI stuff

--- RackSimSoft/src/gui2D/GridComponent.java
+++ RackSimSoft/src/gui2D/GridComponent.java
@@ -6,10 +6,12 @@ public class GridComponent extends RectangleComponent
 {
 	private static final long serialVersionUID = 1L;
 	private final static float gridBorder = 2.5f;
+
 	
 	public GridComponent(Grid grid, double guiCoordinateFactor)
 	{
 		super(grid.getGridID(), (int) (guiCoordinateFactor * grid.getYSize()), (int) (guiCoordinateFactor * grid.getZSize()), gridBorder);
+		
 	}
 	
 	public int getWidth()
--- RackSimSoft/src/gui2D/MainFrame.java
+++ RackSimSoft/src/gui2D/MainFrame.java
@@ -1,7 +1,13 @@
 
 package gui2D;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 /**
  * 
@@ -13,12 +19,12 @@ import javax.swing.JFrame;
  * Der Rahmen f�r das GUI. Auf diesen Rahmen werden alle anderen Komponenten gelegt.
  */
 
-public class MainFrame extends JFrame
+public class MainFrame
 {
-	private static final long serialVersionUID = 1L;
-	final int HEIGHT = 500;
-	final int WIDTH = 1000;
+	final int HEIGHT = 780;
+	final int WIDTH = 1020;
 	final String TITLE = "RackSimSoft";
+	JFrame panel;
 	
 	/**
 	 * Creates a Window in which the Location can be shown.
@@ -26,8 +32,34 @@ public class MainFrame extends JFrame
 	 */
 	public MainFrame()
 	{
-	    this.setSize(WIDTH, HEIGHT);
-	    this.setTitle(TITLE);
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	    JFrame panel = new JFrame();
+	    
+
+	    
+		panel.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		panel.setMinimumSize(new Dimension(WIDTH,HEIGHT));
+	    panel.setSize(WIDTH, HEIGHT);
+	    panel.setTitle(TITLE);
+	    panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setVisible(true);
+        
+        panel.getContentPane().add(new Grid());
+       
 	}
 }
+
+class Grid extends JComponent
+{
+	public void paint(Graphics g)
+	{
+		g.drawRect(10, 10, 440, 380);
+		
+	
+		
+		g.drawRect(460, 10, 80, 380);
+		
+		g.drawRect(550, 10, 440, 380);
+	}
+}
+
