d0da294c35f4719840df47b2b7c3897b53506dd9
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-24 01:25:15 +0100

    rework location part

--- RackSimSoft/src/helper/ReadingFiles.java
+++ RackSimSoft/src/helper/ReadingFiles.java
@@ -78,28 +78,90 @@ public class ReadingFiles {
 		 */
 		Path path = Paths.get(FileName);
 		Location myLocation;
-		Gap gap;
-
 		int line = 0;
 		ArrayList<String> elements = new ArrayList<String>();
-		ArrayList<String> errors = new ArrayList<String>();
+		//ArrayList<String> errors = new ArrayList<String>();
 		try (Scanner scanner =  new Scanner(path, ENCODING.name()))
 		{
 			while (scanner.hasNextLine())
 			{
-				String parts[];
 				String string = scanner.nextLine();
-				//System.out.println(string);
-				if (string.contains(":"))
-				{
-					line++;
-					elements.add(string);
-					//parts = string.split(":");
-				}
+				line++;
+				elements.add(string);
+				//if (string.contains(":"))
+				//{
+				//	line++;
+				//	elements.add(string);
+				//}
 			}
 			System.out.println("Total processed lines: " + line + "\n");
-			//return myLocation;
 	    }
+		//System.out.println(elements);
+		//for (int i = 0; i < elements.size(); i++) {
+		//	System.out.println(elements.toArray()[i]);
+		//}
+		
+		String[] parts = ((String) elements.toArray()[0]).split(";");
+		// Location: myLocation;MM
+		Location.MeasurementUnit measurementUnit;
+	
+		switch (parts[1])
+		{
+			case "MM":
+				measurementUnit = Location.MeasurementUnit.MM;
+				break;
+			case "CM":
+				measurementUnit = Location.MeasurementUnit.CM;
+				break;
+			case "DM":
+				measurementUnit = Location.MeasurementUnit.DM;
+				break;
+			case "M":
+				measurementUnit = Location.MeasurementUnit.M;
+				break;
+			default:
+				measurementUnit = Location.MeasurementUnit.MM;
+		}
+		myLocation = Location.getInstance(parts[0], measurementUnit);
+		
+		for (int i = 0; i + 5 < elements.size(); i= i + 5) {
+			// Gap
+			String[] partsA = ((String) elements.toArray()[5 + i]).split(";");
+			int p = 300;
+			Gap gap = new Gap(partsA[0], p, (p * 2 * 300) + 300 + (p * 300));  // id, width, x coordinate
+			
+			// Columns
+			String[] partsC = ((String) elements.toArray()[2 + i]).split(";");
+			int columnCounter = partsC.length;
+			Column columnArray[] = new Column[columnCounter];
+			
+			int c_coordinate = 0;
+			for (int c = 0; c < partsC.length; c++) {
+				c_coordinate = c_coordinate + c;//partsC[1]
+				columnArray[c] = new Column(partsC[0], 300, c_coordinate);
+			}
+			
+			// Rows
+			String[] partsR = ((String) elements.toArray()[3 + i]).split(";");
+			int rowCounter = partsR.length;
+			Row rowArray[] = new Row[rowCounter];
+
+			int r_coordinate = 0;
+			for (int r = 0; r < partsR.length; r++) {
+				c_coordinate = c_coordinate + r;
+				rowArray[r] = new Row(partsR[0], 300, r_coordinate);
+			}
+
+			// Grid
+			String[] partsG = ((String) elements.toArray()[4 + i]).split(";");
+			for (int g = 0; g < partsG.length; g++) {
+				Grid grid = new Grid(partsG[0], gap, g % 2, columnArray, rowArray, 300);
+				gap.addGrid(grid);
+			}			
+			// Assign gap (with assigned grids) to the location
+			myLocation.addGap(gap);
+		}
+		System.out.println("--> Location created" + "\n");
 	}
 
 	/**
